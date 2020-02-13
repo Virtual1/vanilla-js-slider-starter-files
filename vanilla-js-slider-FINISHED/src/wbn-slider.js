fd024f9deac05cbcceff10ba3a29d39a666067ee
Virtual1 <jessica@virtualangels.co.za>
2020-02-13 10:24:00 +0200

    Completed
    Completed video tutorial as on youtube

--- vanilla-js-slider-FINISHED/src/wbn-slider.js
+++ vanilla-js-slider-FINISHED/src/wbn-slider.js
@@ -1,13 +1,13 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   // CHANGE ONLY THIS
   const SLIDETIME = 500; //ms
   // --------------------------
 
-  const backButton = document.querySelector('.wbn-slider-back-btn');
-  const forwardButton = document.querySelector('.wbn-slider-next-btn');
+  const backButton = document.querySelector(".wbn-slider-back-btn");
+  const forwardButton = document.querySelector(".wbn-slider-next-btn");
   // Select all slides and convert node to array for easy handling
   // const allSlides = Array.from(document.querySelectorAll('.wbn-slide'));
-  const allSlides = [...document.querySelectorAll('.wbn-slide')];
+  const allSlides = [...document.querySelectorAll(".wbn-slide")];
   let clickable = true;
   let active = null;
   let newActive = null;
@@ -16,59 +16,62 @@ document.addEventListener('DOMContentLoaded', () => {
     // Set the CSS transition on the slides to the value we specified in SLIDETIME above
     allSlides.forEach(slide =>
       slide.setAttribute(
-        'style',
+        "style",
         `transition: transform ${SLIDETIME}ms ease;
-                     animation-duration: ${SLIDETIME}ms`,
-      ),
-    );
+                     animation-duration: ${SLIDETIME}ms`
+      )
+    );  
   }
 
   function changeSlide(forward) {
     if (clickable) {
       clickable = false;
-      active = document.querySelector('.active');
+      active = document.querySelector(".active");
       const activeSlideIndex = allSlides.indexOf(active);
 
       if (forward) {
-        console.log('activeSlideIndex: ', activeSlideIndex);
-        console.log('allSlides.length: ', allSlides.length);
-        console.log('new slide: ', (activeSlideIndex + 1) % allSlides.length);
+        console.log("activeSlideIndex: ", activeSlideIndex);
+        console.log("allSlides.length: ", allSlides.length);
+        console.log("new slide: ", (activeSlideIndex + 1) % allSlides.length);
 
         newActive = allSlides[(activeSlideIndex + 1) % allSlides.length];
-        active.classList.add('slideOutLeft');
-        newActive.classList.add('slideInRight', 'active');
+        active.classList.add("slideOutLeft");
+        newActive.classList.add("slideInRight", "active");
       } else {
-        console.log('activeSlideIndex: ', activeSlideIndex);
-        console.log('allSlides.length: ', allSlides.length);
-        console.log('new slide: ', (activeSlideIndex - 1 + allSlides.length) % allSlides.length);
+        console.log("activeSlideIndex: ", activeSlideIndex);
+        console.log("allSlides.length: ", allSlides.length);
+        console.log(
+          "new slide: ",
+          (activeSlideIndex - 1 + allSlides.length) % allSlides.length
+        );
 
         newActive =
           allSlides[
             (activeSlideIndex - 1 + allSlides.length) % allSlides.length
           ];
-        active.classList.add('slideOutRight');
-        newActive.classList.add('slideInLeft', 'active');
+        active.classList.add("slideOutRight");
+        newActive.classList.add("slideInLeft", "active");
       }
     }
   }
-  
+
   allSlides.forEach(slide => {
-    slide.addEventListener('transitionend', e => {
+    slide.addEventListener("transitionend", e => {
       // Check for the old active transition and if clickable is false
       // to not trigger it more than once
       if (slide === active && !clickable) {
         clickable = true;
         // Remove all CSS animation classes on old active
-        active.className = 'wbn-slide';
+        active.className = "wbn-slide";
       }
     });
   });
-  
+
   //Event listeners
-  forwardButton.addEventListener('click', () => {
+  forwardButton.addEventListener("click", () => {
     changeSlide(true);
   });
-  backButton.addEventListener('click', () => {
+  backButton.addEventListener("click", () => {
     changeSlide(false);
   });
 
--- vanilla-js-slider-START-HERE/src/wbn-slider.js
+++ vanilla-js-slider-START-HERE/src/wbn-slider.js
@@ -1,3 +1,66 @@
-document.addEventListener('DOMContentLoaded', () => {
- // START HERE
+document.addEventListener("DOMContentLoaded", () => {
+  // START HERE
+
+  const SLIDETIME = 500; // HALF A SECOND PER SLIDE
+  const backButton = document.querySelector(".wbn-slider-back-btn");
+  const forwardButton = document.querySelector(".wbn-slider-next-btn");
+  const allSlides = [...document.querySelectorAll(".wbn-slide")];
+
+  let clickable = true;
+  let active = null;
+  let newActive = null;
+  /// info: init function
+  function initSlider() {
+    allSlides.forEach(slide => {
+      slide.setAttribute(
+        "style",
+        `transition: transform ${SLIDETIME}ms ease;
+                    animation-duration: ${SLIDETIME}ms
+                    ` /// info: template literals uses backticks
+      );
+    });
+  }
+
+  function changeSlide(forward) {
+    if (clickable) {
+      clickable = false; // we don't want them to be able to click while animating
+      active = document.querySelector(".active");
+      const activeSlideIndex = allSlides.indexOf(active);
+
+      if (forward) {
+        console.log("active slide index: ", activeSlideIndex);
+        console.log("allSlides length: ", allSlides.length);
+        console.log("new Slide: ", (activeSlideIndex + 1) % allSlides.length);
+
+        newActive = allSlides[(activeSlideIndex + 1) % allSlides.length];
+        active.classList.add("slideOutLeft");
+        newActive.classList.add("slideInRight", "active");
+      } else {
+        newActive =
+          allSlides[
+            (activeSlideIndex - 1 + allSlides.length) % allSlides.length
+          ];
+        active.classList.add("slideOutRight");
+        newActive.classList.add("slideInLeft", "active");
+      }
+    }
+  }
+
+  allSlides.forEach(slide => {
+    slide.addEventListener("transitionend", e => {
+      if (slide === active && !clickable) {
+        clickable = true;
+        active.className = "wbn-slide";
+      }
+    });
+  });
+  //Event Listeners
+  forwardButton.addEventListener("click", () => {
+    changeSlide(true);
+  });
+  backButton.addEventListener("click", () => {
+    changeSlide(false);
+  });
+  // init the slider
+  initSlider();
 });
